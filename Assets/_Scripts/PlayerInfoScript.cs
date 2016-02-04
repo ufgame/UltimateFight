fea0e0280719dfcdea7aed3999aeaec785e63363
Kirill <kirill.sviderski@i.ua>
2016-02-04 12:33:39 +0200

    1

---
+++ Assets/_Scripts/PlayerInfoScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerInfoScript : MonoBehaviour {
+
+    public string playerName;
+    public string id;
+
+	// Use this for initialization
+	void Start () {
+        Object.DontDestroyOnLoad(gameObject);
+	}
+
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+            Application.Quit();
+    }
+
+}
